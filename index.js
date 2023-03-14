cbafc3c38ed5d985b217ee85f479f2f5d954d172
teddy wambua <teddywambua5456@gmail.com>
2023-03-14 11:34:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let messagesArray = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      messagesArray.push(message);
+    }
+    return messagesArray;
+  }
+  const messages = writeCards(["Ada", "Brendan", "Ali"], "birthday");
+console.log(messages) 
+
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
